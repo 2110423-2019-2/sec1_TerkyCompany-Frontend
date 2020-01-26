662bcb9dee8279f630d76d846df71a1dd5ea76db
sprkzoff <sprkzoff@gmail.com>
2020-01-26 18:32:44 +0700

    :recycle: refactor code to component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: false,
+
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    if (this.state.isLoading) return null
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+            </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
