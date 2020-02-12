3015245747884b90d19904d01bbfb65e35632055
sprkzoff <sprkzoff@gmail.com>
2020-02-13 04:02:00 +0700

    add intro & slick to homepage

--- src/Components/Homepage/App.js
+++ src/Components/Homepage/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 class App extends Component {
   constructor(props) {
@@ -15,11 +17,50 @@ class App extends Component {
   }
 
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 8000,
+      speed: 1500,
+    }
     if (this.state.isLoading) return null
     console.log("hello Homepage")
     return (
       <div className="App">
-        <h1>Matcher</h1>
+
+        <div className="flex-container" id="intro">
+          <div className="left-intro">
+            <div id="text"><b>Matcher</b> is a workshop matching platform Free for visiting and ease of use for new comers</div>
+          </div>
+          <div className="right-intro">
+          </div>
+        </div>
+        <div id="hot-content">
+          <div>
+            <h2 id="in-trend-head">In trend workshop</h2>
+            <Slider {...settings}>
+              <div>
+                <img id="slick-item" src="test.jpg" />
+              </div>
+              <div>
+                <img id="slick-item" src="test.jpg" />
+              </div>
+              <div>
+                <img id="slick-item" src="test.jpg" />
+              </div>
+              <div>
+                <img id="slick-item" src="test.jpg" />
+              </div>
+              <div>
+                <img id="slick-item" src="test.jpg" />
+              </div>
+            </Slider>
+          </div>
+        </div>
       </div>
     )
   }
