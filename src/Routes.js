c1989ce09def3619dd226f3e3f6660927cf5d0ac
sprkzoff <sprkzoff@gmail.com>
2020-02-19 22:04:50 +0700

    edit title

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import App from './Components/Homepage/App';
 import About from './Components/About';
 import React, { Component } from 'react';
-
+import { Helmet } from 'react-helmet';
 import WorkshopDetail from './Components/Workshop_detail/WorkshopDetail';
 
 import Menubar from './Components/Menubar/Menubar';
@@ -13,11 +13,14 @@ class Routes extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>Matcher</title>
+                </Helmet>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-                <Menubar/>
+                <Menubar />
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/" component={App} />
