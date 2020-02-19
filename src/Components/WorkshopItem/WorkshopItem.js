315ddd7651e7441aba77f30cdb403f96b69de24c
sprkzoff <sprkzoff@gmail.com>
2020-02-20 01:05:21 +0700

    finish workshop item

--- src/Components/WorkshopItem/WorkshopItem.js
+++ src/Components/WorkshopItem/WorkshopItem.js
@@ -4,23 +4,34 @@ import "./WorkshopItem.css";
 class WorkshopItem extends Component {
     constructor(props) {
         super(props)
+        console.log("check item >", this.props.item.id)
         this.state = {
             workshop_id: this.props.item.id,
             image_name: "test.jpg",
             workshop_name: this.props.item.name,
-            workshop_detail: this.props.item.description.slice(0,60),
-            start: this.props.item.startTime,
+            workshop_detail: this.props.item.description.slice(0, 60),
+            start: this.convertTimeStampToTime(this.props.item.startTime),
             place: this.props.item.place,
-            end: this.props.item.endTime,
+            end: this.convertTimeStampToTime(this.props.item.endTime),
             owner: this.props.item.speakerName,
-            
+
             isLoading: false,
 
         }
+        this.goto = this.goto.bind(this)
+    }
+
+    convertTimeStampToTime = (timeStamp) => {
+        let time = timeStamp.slice(11, 16)
+        let date = timeStamp.slice(0, 10)
+        let timeAndDate = { "time": time, "date": date }
+        return timeAndDate
     }
 
     goto() {
-        window.location.assign('/workshop-detail')
+        // let check = this.props.item.id
+        // console.log(this.state.workshop_id)
+        window.location.assign('/workshop-detail/' + this.state.workshop_id)
     }
 
     componentDidMount() {
@@ -33,7 +44,7 @@ class WorkshopItem extends Component {
                 <img src={this.state.image_name} height="200" width="300"></img>
                 <h3>{this.state.workshop_name}</h3>
                 <p>{this.state.workshop_detail}</p>
-                <h6>{this.state.start} - {this.state.end}</h6>
+                <h6>{this.state.start.date} {this.state.start.time} to {this.state.end.date} {this.state.end.time}</h6>
                 <h6>@{this.state.place}</h6>
                 <h5>Create by {this.state.owner}</h5>
             </div>
