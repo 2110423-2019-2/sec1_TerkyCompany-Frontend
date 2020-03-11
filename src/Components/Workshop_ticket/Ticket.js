c47ab87074f3df334a4e8a366f5929904ec14a1e
GottaK <chan_tham@hotmail.com>
2020-03-11 14:06:10 +0700

    css update

--- src/Components/Workshop_ticket/Ticket.js
+++ src/Components/Workshop_ticket/Ticket.js
@@ -10,60 +10,55 @@ class Ticket extends React.Component {
         this.state = {
             isLoading : false,
             data :{
-                workShopName : 'workShopName',
-                workShopPic : 'PicURL',
-                stime : 'start time',
-                etime : 'end time',
+                workShopName : 'Workshop\'s name',
+                workShopPic : 'Picture URL',
+                stime : 'start',
+                etime : 'end',
                 place : 'place',
-                holderName : 'holder',
-                description : 'descs'
+                holderName : 'holder name',
                 }
         }
     }
 
-    componentDidMount() {
-        console.log('okkkkk');
-        const { ID } = this.props.match.params
-        axios.get('http://localhost:3001/workshops/' + ID).then(res => {
-            console.log("from workshop > ", res.data)
-            this.setState({
-                data: {
-                    workShopName : res.data.name,
-                    workShopPic : res.data.pictureURL,
-                    date : "",
-                    stime : this.convertTimeStampToTime(res.data.startTime).time,
-                    etime : this.convertTimeStampToTime(res.data.endTime).time,
-                    place : res.data.place,
-                    holderName : "Miw",//read cookie,
-                    description : "dasdasas"
-                }
-            })
-        })
-    }
-    convertTimeStampToTime = (timeStamp) => {
-        let time = timeStamp.slice(11, 16)
-        let date = timeStamp.slice(0, 10)
-        let timeAndDate = { "time": time, "date": date }
-        return timeAndDate
-    }
+    // componentDidMount() {
+    //     console.log('okkkkk');
+    //     const { ID } = this.props.match.params
+    //     axios.get('http://localhost:3001/workshops/' + ID).then(res => {
+    //         console.log("from workshop > ", res.data)
+    //         this.setState({
+    //             data: {
+    //                 workShopName : res.data.name,
+    //                 workShopPic : res.data.pictureURL,
+    //                 date : "",
+    //                 stime : this.convertTimeStampToTime(res.data.startTime).time,
+    //                 etime : this.convertTimeStampToTime(res.data.endTime).time,
+    //                 place : res.data.place,
+    //                 holderName : "Miw",//read cookie,
+    //                 description : "dasdasas"
+    //             }
+    //         })
+    //     })
+    // }
+    // convertTimeStampToTime = (timeStamp) => {
+    //     let time = timeStamp.slice(11, 16)
+    //     let date = timeStamp.slice(0, 10)
+    //     let timeAndDate = { "time": time, "date": date }
+    //     return timeAndDate
+    // }
     render() {
         return (
         <div id='ticket'>
             <div id='workshopPic'><img src={this.state.data.workShopPic}></img></div>
             <div id='text-box'>
-                <div id='workshopName'>{this.state.data.workShopName}</div>
+                <div id='workshopName'><h2>{this.state.data.workShopName}</h2></div>
                 <div className='desc'>
-                    <div>
-                        <span>{this.state.data.stime}</span> - <span>{this.state.data.etime}</span>
-                    </div>
-                    <div>{this.state.data.place}</div>
-                    <div>{this.state.data.desc}</div>
-                    <div id="holderName">{this.state.data.holderName}</div>
+                    <p>from {this.state.data.stime} to {this.state.data.etime}</p>
+                    <p>{this.state.data.place}</p>
                 </div>
-                <div id="holderName">{this.props.holderName}</div>
+                <div id="holderName"><h3>{this.state.data.holderName}</h3></div>
             </div>
             <div id='qrCode'>
-                <Qrcode value="pitaratethi" size={150}  bgColor="#E48D26" />
+                <Qrcode value="FUCK U" size={150}  bgColor="#E48D26" fgColor="#FFF0D9" />
             </div>
         </div>
         )
