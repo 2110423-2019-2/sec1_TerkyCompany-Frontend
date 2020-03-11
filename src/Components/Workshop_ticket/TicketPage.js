1b25b685cadaafae4131a9ac175c0b9d1503ff54
GottaK <chan_tham@hotmail.com>
2020-03-11 12:36:46 +0700

    debug

--- src/Components/Workshop_ticket/TicketPage.js
+++ src/Components/Workshop_ticket/TicketPage.js
@@ -2,6 +2,7 @@ import React from 'react' ;
 import ReactToPDF from 'react-to-print' ;
 import Ticket from './Ticket';
 import './TicketPage.css' ;
+import axios from "axios";
 
 class TicketPage extends React.Component {
     constructor(props) {
@@ -19,7 +20,31 @@ class TicketPage extends React.Component {
                 }
         }
     }
-
+    componentDidMount() {
+        console.log('okkkkk');
+        const { ID } = this.props.match.params
+        axios.get('http://localhost:3001/workshops/' + ID).then(res => {
+            console.log("from workshop > ", res.data)
+            this.setState({
+                data: {
+                    workShopName : res.data.name,
+                    workShopPic : res.data.pictureURL,
+                    date : "",
+                    stime : this.convertTimeStampToTime(res.data.startTime).time,
+                    etime : this.convertTimeStampToTime(res.data.endTime).time,
+                    place : res.data.place,
+                    holderName : "Miw",//read cookie,
+                    description : "dasdasas"
+                }
+            })
+        })
+    }
+    convertTimeStampToTime = (timeStamp) => {
+        let time = timeStamp.slice(11, 16)
+        let date = timeStamp.slice(0, 10)
+        let timeAndDate = { "time": time, "date": date }
+        return timeAndDate
+    }
     render() {
         return (
             <div id='container'>
