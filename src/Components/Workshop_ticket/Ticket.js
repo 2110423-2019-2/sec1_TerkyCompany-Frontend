b3db6fc555443001ec3be55b4ef820a3c5cf8ecb
GottaK <chan_tham@hotmail.com>
2020-03-11 14:06:47 +0700

    debug

--- src/Components/Workshop_ticket/Ticket.js
+++ src/Components/Workshop_ticket/Ticket.js
@@ -20,31 +20,31 @@ class Ticket extends React.Component {
         }
     }
 
-    // componentDidMount() {
-    //     console.log('okkkkk');
-    //     const { ID } = this.props.match.params
-    //     axios.get('http://localhost:3001/workshops/' + ID).then(res => {
-    //         console.log("from workshop > ", res.data)
-    //         this.setState({
-    //             data: {
-    //                 workShopName : res.data.name,
-    //                 workShopPic : res.data.pictureURL,
-    //                 date : "",
-    //                 stime : this.convertTimeStampToTime(res.data.startTime).time,
-    //                 etime : this.convertTimeStampToTime(res.data.endTime).time,
-    //                 place : res.data.place,
-    //                 holderName : "Miw",//read cookie,
-    //                 description : "dasdasas"
-    //             }
-    //         })
-    //     })
-    // }
-    // convertTimeStampToTime = (timeStamp) => {
-    //     let time = timeStamp.slice(11, 16)
-    //     let date = timeStamp.slice(0, 10)
-    //     let timeAndDate = { "time": time, "date": date }
-    //     return timeAndDate
-    // }
+    componentDidMount() {
+        console.log('okkkkk');
+        const { ID } = this.props.match.params
+        axios.get('http://localhost:3001/workshops/' + ID).then(res => {
+            console.log("from workshop > ", res.data)
+            this.setState({
+                data: {
+                    workShopName : res.data.name,
+                    workShopPic : res.data.pictureURL,
+                    date : "",
+                    stime : this.convertTimeStampToTime(res.data.startTime).time,
+                    etime : this.convertTimeStampToTime(res.data.endTime).time,
+                    place : res.data.place,
+                    holderName : "Miw",//read cookie,
+                    description : "dasdasas"
+                }
+            })
+        })
+    }
+    convertTimeStampToTime = (timeStamp) => {
+        let time = timeStamp.slice(11, 16)
+        let date = timeStamp.slice(0, 10)
+        let timeAndDate = { "time": time, "date": date }
+        return timeAndDate
+    }
     render() {
         return (
         <div id='ticket'>
