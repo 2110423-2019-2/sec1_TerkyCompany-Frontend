949afc74ba7e6e76f8dd6ba0326f44f4fd9208df
sprkzoff <sprkzoff@gmail.com>
2020-03-28 01:48:49 +0700

    waiting for BE fix & implement goto ticket next

--- src/Components/Workshop_detail/Button.js
+++ src/Components/Workshop_detail/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Button.css';
 import Script from 'react-load-script';
+import Axios from 'axios';
 
 
 let OmiseCard
@@ -30,23 +31,37 @@ class Button extends React.Component {
             publicKey: 'pkey_test_5jat0453twxxl7wh47y',
             currency: 'thb',
             image: 'YOUR_LOGO_URL',
-            frameLabel : 'Matcher',
-            submitLabel : 'PAY ',
-            buttonLabel : 'Pay with Omise',
+            frameLabel: 'Matcher',
+            submitLabel: 'PAY ',
+            buttonLabel: 'Pay with Omise',
         })
     }
 
     omiseCardHandler = () => {
         OmiseCard.open({
-            amount: this.props.workshop['cost'] ,
+            amount: this.props.workshop['cost'] * 100,
             submitFormTarget: '#credit-card',
             onCreateTokenSuccess: (token) => {
                 console.log(token)
+                Axios.post("http://localhost:3001/payment/create", { 'email': 'test@gmail.com', 'name': this.props.username, 'amount': this.props.workshop['cost'], 'token': token }).then(res => {
+
+                    if (res.data.status === 'successful') {
+                        console.log(res.data.status)
+                        Axios.post("http://localhost:3001/books/create", {
+                            "workshop": this.props.workshop['id'],
+                            "memberT": this.props.username,
+                            "hasParticipated": false,
+                            "transactionDetail": "success"
+                        })
+
+                        // goto ticket page next
+                    }
+                })
             },
             onFormClosed: () => {
 
             },
-          })
+        })
     }
 
     handleClick = e => {
@@ -70,7 +85,7 @@ class Button extends React.Component {
             <div>
                 <Script
                     url="https://cdn.omise.co/omise.js.gz"
-                    
+
                     onLoad={this.handleLoad}
                 />
                 <form >
