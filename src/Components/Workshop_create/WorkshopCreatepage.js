1b1e10f00315ed72a4bd6b46de59290b3d4e0272
Tana Chanchiew <terktana18@gmail.com>
2020-04-27 02:34:51 +0700

    Fix edit & create

--- src/Components/Workshop_create/WorkshopCreatepage.js
+++ src/Components/Workshop_create/WorkshopCreatepage.js
@@ -36,29 +36,29 @@ class WorkshopCreatePage extends React.Component {
     }
     render() {
         if(this.state.isLoading) return null
-        if (document.cookie === ""){ 
-            window.alert("Please login first");
-            window.location.assign('/login');
-            return null
-        }
-        if (this.state.role != "owner" && this.state.role != "admin") {
-            console.log("cookie");
-            console.log(this.state);
-            window.alert("Participant can't create a workshop, please login as an Owner");
-            window.location.assign('/');
-            return null
-        }
+        // if (document.cookie === ""){ 
+        //     window.alert("Please login first");
+        //     window.location.assign('/login');
+        //     return null
+        // }
+        // if (this.state.role != "owner" && this.state.role != "admin") {
+        //     console.log("cookie");
+        //     console.log(this.state);
+        //     window.alert("Participant can't create a workshop, please login as an Owner");
+        //     window.location.assign('/');
+        //     return null
+        // }
         return (
-            <div id="container">
+            <div className="flex-container" id="flex-container">
                 <div id="sidebar">
                     <SideBar content={this.state.content}/>
                 </div>
                 <div className="show-list">
                     <h1>Create workshop</h1>
-                    <div className="dropdown-divider"></div>
-                    <div>
-                        <CreateForm id="form" username={this.state.username} tags={this.state.tags}/>
-                    </div>
+                <div className="dropdown-divider"></div>
+                <div>
+                    <CreateForm id="form" username={this.state.username} tags={this.state.tags}/>
+                </div>
                 </div>
                 <div id="sidebar">
                     <SideBar content={this.state.content}/>
--- src/Components/Workshop_editor/WorkshopEditPage.js
+++ src/Components/Workshop_editor/WorkshopEditPage.js
@@ -51,29 +51,29 @@ class WorkshopEditPage extends React.Component {
 
     render() {
         if(this.state.isLoading) return null
-        if (document.cookie === ""){ 
-            window.alert("Please login first");
-            window.location.assign('/login');
-            return null
-        }
-        if (this.state.role != "owner" && this.state.role != "admin") {
-            console.log("cookie");
-            console.log(this.state);
-            window.alert("Participant can't edit a workshop, please login as an Owner");
-            window.location.assign('/');
-            return null
-        }
+        // if (document.cookie === ""){ 
+        //     window.alert("Please login first");
+        //     window.location.assign('/login');
+        //     return null
+        // }
+        // if (this.state.role != "owner" && this.state.role != "admin") {
+        //     console.log("cookie");
+        //     console.log(this.state);
+        //     window.alert("Participant can't edit a workshop, please login as an Owner");
+        //     window.location.assign('/');
+        //     return null
+        // }
         return (
-            <div id="container">
+            <div className="flex-container" id="flex-container">
                 <div id="sidebar">
                     <SideBar content={this.state.content}/>
                 </div>
                 <div className="show-list">
                     <h1>Edit Workshop</h1>
-                    <div className="dropdown-divider"></div>
-                    <div>
-                        <EditForm id="form" workshopid={this.state.workshopID} urlUsername={this.state.urlname}/>
-                    </div>
+                <div className="dropdown-divider"></div>
+                <div>
+                    <EditForm id="form" workshopid={this.state.workshopID} urlUsername={this.state.urlname}/>
+                </div>
                 </div>
                 <div id="sidebar">
                     <SideBar content={this.state.content}/>
