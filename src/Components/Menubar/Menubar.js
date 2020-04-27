849bf21b8c3995cf453db6d74c33bbd5b26e4de3
sprkzoff <sprkzoff@gmail.com>
2020-04-27 22:25:17 +0700

    add management for admin

--- src/Components/Menubar/Menubar.js
+++ src/Components/Menubar/Menubar.js
@@ -7,13 +7,36 @@ class Menubar extends Component {
         super(props)
         this.state = {
             isLoading: false,
+            username: '',
+            role:''
 
         }
     }
 
 
-    componentDidMount() {
+    componentWillMount() {
         console.log(document.cookie)
+        //format cookie
+        let spl = document.cookie.split(';')
+        let ck = {}
+        let s=0
+        for(let i=0 ;i< spl.length ; i++)
+        {
+            let temp = spl[i].split('=')
+            // console.log('temp: ',temp)
+            ck[temp[0].trim()]=temp[1]
+            if(temp[0].trim() === 'username' || temp[0].trim() === 'userType')
+                s+=1 
+
+        }
+        
+        if(s===2) {
+            this.setState({
+                isLoading: false,
+                username: ck['username'],
+                role: ck['userType']
+            })
+        }
     }
 
     render() {
@@ -46,35 +69,72 @@ class Menubar extends Component {
                 </div>
             )
         else
-            return (
-                <div className="Menubar">
-                    <nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
-                        <a class="navbar-brand" href="/">
-                            {/* <img src="./../Homepage/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="" /> */}
-                            Matcher
-                        </a>
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/workshoplist">My Workshop</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/browse">Browse</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
-                            </li>
-                            <form class="form-inline my-2 my-lg-0">
-                                <button class="btn btn-outline-light my-2 my-sm-0" onClick={() => {
-                                    //clear cookie somehow
-                                    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                                    document.cookie = "userType=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                                    window.location.assign('/')
-                                } }>Logout</button>
-                            </form>
-                        </ul>
-                    </nav>
-                </div>
-            )
+            if(this.state.role === 'admin') {
+                return (
+                    <div className="Menubar">
+                        <nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
+                            <a class="navbar-brand" href="/">
+                                {/* <img src="./../Homepage/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="" /> */}
+                                Matcher
+                            </a>
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/workshoplist">My Workshop</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/browse">Browse</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/management/user">Management</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">About</a>
+                                </li>
+                                <form class="form-inline my-2 my-lg-0">
+                                    <button class="btn btn-outline-light my-2 my-sm-0" onClick={() => {
+                                        //clear cookie somehow
+                                        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                                        document.cookie = "userType=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                                        window.location.assign('/')
+                                    } }>Logout</button>
+                                </form>
+                            </ul>
+                        </nav>
+                    </div>
+                )
+            }
+            else {
+                return (
+                    <div className="Menubar">
+                        <nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
+                            <a class="navbar-brand" href="/">
+                                {/* <img src="./../Homepage/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="" /> */}
+                                Matcher
+                            </a>
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/workshoplist">My Workshop</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/browse">Browse</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">About</a>
+                                </li>
+                                <form class="form-inline my-2 my-lg-0">
+                                    <button class="btn btn-outline-light my-2 my-sm-0" onClick={() => {
+                                        //clear cookie somehow
+                                        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                                        document.cookie = "userType=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                                        window.location.assign('/')
+                                    } }>Logout</button>
+                                </form>
+                            </ul>
+                        </nav>
+                    </div>
+                )
+            }
+            
     }
 }
 
